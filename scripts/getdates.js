70583fdadf457973c74775fdd281f634216e3298
GarionsShadow <Guardof6@gmail.com>
2024-11-08 11:46:48 -0700

    fix

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,5 @@
 const lastupdated = document.lastModified
-const copyrightYear = document.querySelector("#year")
+const copyrightYear = 2024
+ document.getElementById("cyear").innerHTML = copyrightYear
+
+ document.getElementById("lastu").innerHTML = lastupdated
